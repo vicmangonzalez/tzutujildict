5e02dbb68ec83f9625e4c21bd9d53ee5abdfee4c
vicmangonzalez <gonznake@gmail.com>
2022-02-17 13:54:59 -0600

    Pruebas de audio.

--- js/search.js
+++ js/search.js
@@ -18,11 +18,24 @@ $('form').on('submit', function (e) {
          "<div class=\"card bg-light my-3 col-md-4 p-2\">" +
          "<div class=\"card-header\"> #" + p.id + "</div>" +
          "<div class=\"card-body\">" +
-         "<h5 class=\"card-title\">"+ p.espaniol + " - " + p.tzutujil + "</h5>" +
+         "<h5 class=\"card-title\">" + p.espaniol + " - " + p.tzutujil + "</h5>" +
          "<p class=\"card-text\">" + p.exes + "<br>" + p.extzu + "</p>" +
+         "<button type=\"button\" class=\"btn btn-primary btnAudio\" data-toggle=\"modal\" data-target=\"#audioModal\">" +
+         "<i class=\"fas fa-volume-up\"></i>" +
+         "</button>" +
+         " <input type=\"hidden\" value=\"" + p.audiosrc + "\">" +
          "</div>" +
          "</div>"
       )
+   });
+
+   $('.btnAudio').click(function () {
+      $source = $(this).next('input').val();
+      var audio = $("#reproductorAudio");
+      $('#audioElement').attr("src", $source)
+      audio[0].pause();
+      audio[0].load();
+      audio[0].oncanplaythrough = audio[0].play();
    });
 
 });
