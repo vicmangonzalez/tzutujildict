0990a834accf58e87d2c7d30940239d17bf6da1f
vicmangonzalez <gonznake@gmail.com>
2022-02-17 16:30:11 -0600

    Mejoras de búsqueda y JSON nuevo.

--- js/search.js
+++ js/search.js
@@ -7,36 +7,48 @@ fetch('db/palabras.json')
 $('form').on('submit', function (e) {
    e.preventDefault();
    let texto = $('#buscar').val();
+   $('#cards').empty();
+   $('#errorMensaje').empty();
 
    // console.log(palabras)
-   let nuevosElementos = filterItems(texto);
 
-   $('#cards').empty();
+   if (texto.includes("'")) {
+      $('#cards').html("<p class=\"text-primary text-center\">Parece que estás intentando buscar una palabra en tzutujil, te recomendamos comprar el libro en la Academia de Lenguas Mayas más cercana para tener el diccionario completo.</p>")
+   } else if (texto.length != 0) {
+      let nuevosElementos = filterItems(texto);
 
-   nuevosElementos.forEach(p => {
-      $('#cards').append(
-         "<div class=\"card bg-light my-3 col-md-4 p-2\">" +
-         "<div class=\"card-header\"> #" + p.id + "</div>" +
-         "<div class=\"card-body\">" +
-         "<h5 class=\"card-title\">" + p.espaniol + " - " + p.tzutujil + "</h5>" +
-         "<p class=\"card-text\">" + p.exes + "<br>" + p.extzu + "</p>" +
-         "<button type=\"button\" class=\"btn btn-primary btnAudio\" data-toggle=\"modal\" data-target=\"#audioModal\">" +
-         "<i class=\"fas fa-volume-up\"></i>" +
-         "</button>" +
-         " <input type=\"hidden\" value=\"" + p.audiosrc + "\">" +
-         "</div>" +
-         "</div>"
-      )
-   });
+      nuevosElementos.forEach(p => {
+         $('#cards').append(
+            "<div class=\"card bg-light my-3 col-md-4 p-2\">" +
+            "<div class=\"card-header\"> #" + p.id + "</div>" +
+            "<div class=\"card-body\">" +
+            "<h5 class=\"card-title\">" + p.espaniol + " - " + p.tzutujil + "</h5>" +
+            "<p class=\"card-text\">" + p.exes + "<br>" + p.extzu + "</p>" +
+            "<button type=\"button\" class=\"btn btn-primary btnAudio\" data-toggle=\"modal\" data-target=\"#audioModal\">" +
+            "<i class=\"fas fa-volume-up\"></i>" +
+            "</button>" +
+            " <input type=\"hidden\" value=\"" + p.audiosrc + "\">" +
+            " <input type=\"hidden\" value=\"" + p.voice + "\">" +
+            "</div>" +
+            "</div>"
+         )
+      });
 
-   $('.btnAudio').click(function () {
-      $source = $(this).next('input').val();
-      var audio = $("#reproductorAudio");
-      $('#audioElement').attr("src", $source)
-      audio[0].pause();
-      audio[0].load();
-      audio[0].oncanplaythrough = audio[0].play();
-   });
+      $('.btnAudio').click(function () {
+         $source = $(this).next('input').val();
+         $voz = $(this).next('input').next('input').val();
+
+         $('#laVoz').text($voz)
+
+         var audio = $("#reproductorAudio");
+         $('#audioElement').attr("src", $source)
+         audio[0].pause();
+         audio[0].load();
+         audio[0].oncanplaythrough = audio[0].play();
+      });
+   } else {
+      $('#errorMensaje').html("<p class=\"text-danger text-right\">Debes escribir al menos una letra.</p>")
+   }
 
 });
 
